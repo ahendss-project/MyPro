024aa88e91f11bf9b03d3712d23025d08a3aaa68
ahendss-project <126519217+ahendss-project@users.noreply.github.com>
2023-02-28 05:56:55 +0800

    Add new text for test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App by Agus Endrawan"/>
+  <HelloWorld msg="Welcome to Your Vue.js App by Agus Endrawan New"/>
 </template>
 
 <script>
