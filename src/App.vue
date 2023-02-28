16f3e99c3908bf663a7774e98bd6469abb84f944
ahendss-project <126519217+ahendss-project@users.noreply.github.com>
2023-02-28 14:21:24 +0800

    Update Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App by Agus Endrawan New"/>
+  <HelloWorld msg="Welcome to Your Vue.js App "/>
 </template>
 
 <script>
+import Header from './components/AppHeader.vue'
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
     HelloWorld
+    
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="header">
+      <h1>{{ pageTitle }}</h1>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: "app-header",
+    props: {
+      pageTitle: {
+        type: String,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #f2f2f2;
+    border-bottom: 1px solid #ccc;
+  }
+  
+  nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+  }
+  
+  nav ul li {
+    font-size: 1.2rem;
+  }
+  </style>
